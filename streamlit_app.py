1d2d92e8709f76388a18f212f5cbdc8badf34df0
bcolbert <brett.colbert@gmail.com>
2023-09-27 17:02:42 -0700

    Update streamlit_app.py
    added avocado toast and icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled free-range egg')
-
-
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avocado toast')
